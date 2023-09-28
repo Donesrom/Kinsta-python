39a1a85f4c60c9e291a117ed6da401a54a788408
Donesrom <donesrom@gmail.com>
2023-09-28 13:28:53 +0300

    Added python script to app

---
+++ disk_space-checker.py
@@ -0,0 +1,42 @@
+import psutil
+import time
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+WEBHOOK = os.getenv("WEBHOOK")
+
+
+def get_disk_usage():
+    #Gets the disk usage on the persistent storage.
+    root_partition = "var/lib/data"
+    usage = psutil.disk_usage(root_partition)
+
+    total_disk_space = usage.total
+    
+    return {
+	"total": usage.total / (1024 ** 3),
+	"used": usage.used / (1024 ** 3),
+	"free": usage.free / (1024 ** 3)
+  }
+
+def send_slack_alert(message):
+  #Sends an alert to Slack via a webhook.
+  requests.post(WEBHOOK, json={"text": message})
+
+def main():
+    #The main function.
+    threshold = 50
+    disk_usage = get_disk_usage()
+    free_space = disk_usage["free"]
+
+    if free_space < threshold:
+        message = "Warning: Free disk space is less than {} GB".format(threshold)
+        send_slack_alert(message)
+        print(message)
+
+if __name__ == "__main__":
+  main()
