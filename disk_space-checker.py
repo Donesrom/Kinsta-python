568bfddbd0c6303a54e44e98695b105c91947edb
Donesrom <76392098+Donesrom@users.noreply.github.com>
2023-09-28 14:19:05 +0300

    Update disk_space-checker.py tentative

--- disk_space-checker.py
+++ disk_space-checker.py
@@ -12,7 +12,7 @@ WEBHOOK = os.getenv("WEBHOOK")
 
 def get_disk_usage():
     #Gets the disk usage on the persistent storage.
-    root_partition = "var/lib/data"
+    root_partition = "/space/new"
     usage = psutil.disk_usage(root_partition)
 
     total_disk_space = usage.total
